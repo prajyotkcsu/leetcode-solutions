15f8892379d588d1b2f833987aaea730e087b977
Prajyot Karnik <154280801+prajyotkcsu@users.noreply.github.com>
2024-01-04 11:38:46 -0600

    Update SmallesLetterGreaterThanTarget.java

--- src/SmallesLetterGreaterThanTarget.java
+++ src/SmallesLetterGreaterThanTarget.java
@@ -1,1 +1,15 @@
+input={'a','b','f','g'}
+target='c'
 
+filter the array for elements greater than the target letter(ascii) 
+put these filtered letters into a sorted sorted array
+return first element in that array
+
+  class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char[] filtered=new String(letters).chars().filter(ch-> ch>target).sorted().toArray();
+    if(filtered.length>0){
+        return filtered[0];
+    }
+    }
+}
