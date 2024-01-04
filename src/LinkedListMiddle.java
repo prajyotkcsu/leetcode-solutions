954c21457564e2392a561aa1bcd1ca5945794ad5
Prajyot Karnik <154280801+prajyotkcsu@users.noreply.github.com>
2024-01-04 10:32:21 -0600

    Update LinkedListMiddle.java

--- src/LinkedListMiddle.java
+++ src/LinkedListMiddle.java
@@ -1,1 +1,18 @@
+public class ListNode{
+  int val;
+  ListNode next=next;
+  ListNode(int val){
+    this.val=val;
+  }
 
+  public ListNode middle(ListNode head){
+    ListNode slow=head;
+    ListNode fast=head;
+    while(fast!=null && fast.next!=null){
+      slow=slow.next;
+      fast=fast.next.next;
+  }
+    //fast reached end of list
+    return slow; //slow is in the middle.
+
+}
