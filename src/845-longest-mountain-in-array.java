f3afa79fe18ce217f74cc6a1fdad3d9ed8cf917b
Prajyot Karnik <154280801+prajyotkcsu@users.noreply.github.com>
2024-03-05 23:10:48 -0600

    Create 845-longest-mountain-in-array.java

---
+++ src/845-longest-mountain-in-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestMountain(int[] arr) {
+        int maxLen=0, left=0, right=0;
+        for(int i=1;i<arr.length-1;i++){
+            if(isPeak(arr, i)){
+                left=i;
+                right=i;
+                while(left>0 && !isValley(arr, left)){
+                    left--;
+                }
+                while(right<arr.length-1 && !isValley(arr, right)){
+                    right++;
+                }
+                maxLen=Math.max(maxLen,right-left+1);
+            }
+        }       
+        return maxLen;
+}
+    boolean isPeak(int[] arr, int peak){
+          if(arr[peak]>arr[peak-1] && arr[peak]>arr[peak+1]){
+              return true;
+          }
+          return false;
+      }
+       boolean isValley(int[] arr, int v){
+          if(arr[v]<arr[v-1] && arr[v]<arr[v+1])return true;
+          return false;
+      }
+}
