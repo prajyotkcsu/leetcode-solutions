082f1b68e85353ccaac02bd33338bb85cfae6ba5
Prajyot Karnik <154280801+prajyotkcsu@users.noreply.github.com>
2024-05-16 22:44:46 -0500

    Update 739-daily-temperatures.java

--- src/739-daily-temperatures.java
+++ src/739-daily-temperatures.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] out=new int[temperatures.length];
-        //create a stack
-        Stack<int[]> stack=new Stack<>();
-//70  ....71
-        //iterate and if stack is empty or currentTemp is lesser than the peek() temp, push() temps along with their index in the stack.
+        Stack<Integer> stack=new Stack<>();
+int prevDay=0;
         for(int currDay=0;currDay<temperatures.length;currDay++){
-            //until you find a warmer day you push days in the stack
-            while(!stack.isEmpty() && stack.peek()[0]<temperatures[currDay]){
-                int colderDay=stack.pop()[1]; //pop the colder day and get it's index
-                out[colderDay]=currDay-colderDay; //updating the difference between warmer and colder day.
+            while(!stack.isEmpty() && temperatures[stack.peek()]<temperatures[currDay]){
+                prevDay=stack.pop();
+                out[prevDay]=currDay-prevDay;
             }
-            stack.push(new int[]{temperatures[currDay],currDay});
-        }
-        //pop() currentTemp index-popped temp index-> answer for the popped temp index in the output
-
-
-        return out;
+            stack.push(currDay);   
     }
+    return out;
 }
+}
+
+#Appraoch
+build decreasing monotonoic stack meaning stack smaller numbers as you go up.
+the reason being when a warmer day occurs below colder days affect.
