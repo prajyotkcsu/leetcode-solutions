b913b5280383fbd2b60f460c4324bb7ef2718e17
Prajyot Karnik <154280801+prajyotkcsu@users.noreply.github.com>
2024-08-30 20:14:04 -0500

    Create SubsetsII.java

---
+++ src/neetcode/backtracking/SubsetsII.java
@@ -0,0 +1,21 @@
+/*
+You are given an array nums of integers, which that may contain duplicates. Return all possible subsets.
+
+The solution must not contain duplicate subsets. You may return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,1]
+
+Output: [[],[1],[1,2],[1,1],[1,2,1],[2]]
+Example 2:
+
+Input: nums = [7,7]
+
+Output: [[],[7], [7,7]]
+Constraints:
+
+1 <= nums.length <= 11
+-20 <= nums[i] <= 20
+
+*/
